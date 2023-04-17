f6dd1582f5fc3e6866e95e2fb035e238170f413b
mauro <mauro@genuino.world>
2023-04-17 10:04:31 +0200

    Root endpoint.

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-console.log("Hello World!");
+import express from "express";
+import meta from "../package.json";
+
+const app = express();
+
+app.get("/", (_req, res) => {
+  res.json({
+    name: meta.name,
+    version: meta.version,
+  });
+});
+
+app.listen(5000, () => console.log("Server ready at port 5000"));
