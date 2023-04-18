154984b1bbec45a60e4820ec792a558e47744e9b
mauro <mauro@genuino.world>
2023-04-18 12:03:00 +0200

    Implemented and tested loginUser.

--- src/user/loginUser.test.ts
+++ src/user/loginUser.test.ts
@@ -1,5 +1,65 @@
+import { JwtPayload, verify } from "jsonwebtoken";
+import { loginUser } from "./loginUser";
+import { signUpUser } from "./signUpUser";
+import { withDatabase } from "../withDatabase";
+import { ServerError } from "../ServerError";
+
+const secret = process.env["AUTH_TOKEN_SECRET"];
+
 describe("loginUser", () => {
-  it.todo("should log a user in");
-  it.todo("should handle not found users (wrong email)");
-  it.todo("should handle wrong password");
+  it("should log a user in", async () => {
+    await signUpUser(
+      "Login happy path",
+      "login.happy.path@example.com",
+      "S0m3P4ss!"
+    );
+
+    const user = await withDatabase((db) =>
+      db.user.findOne({ where: { email: "login.happy.path@example.com" } })
+    );
+
+    const tokens = await loginUser("login.happy.path@example.com", "S0m3P4ss!");
+
+    const accessToken = verify(tokens.access, secret!, {
+      issuer: "api",
+      subject: "access",
+    }) as JwtPayload;
+
+    const refreshToken = verify(tokens.refresh, secret!, {
+      issuer: "api",
+      subject: "refresh",
+    }) as JwtPayload;
+
+    expect(accessToken["id"]).toBe(user!.dataValues.id);
+    expect(accessToken.iss).toBe("api");
+    expect(accessToken.sub).toBe("access");
+    expect(accessToken.exp).toBeGreaterThan(Date.now() / 1000);
+
+    expect(refreshToken["id"]).toBe(user!.dataValues.id);
+    expect(refreshToken.iss).toBe("api");
+    expect(refreshToken.sub).toBe("refresh");
+    expect(refreshToken.exp).toBeGreaterThan(Date.now() / 1000);
+  });
+
+  it("should handle not found users (wrong email)", async () => {
+    await expect(
+      loginUser("login.not.found@example.com", "whatever")
+    ).rejects.toEqual(
+      new ServerError(401, "Invalid email address or password")
+    );
+  });
+
+  it("should handle wrong password", async () => {
+    await signUpUser(
+      "Login wrong password",
+      "login.wrong.password@example.com",
+      "S0m3P4ss!"
+    );
+
+    await expect(
+      loginUser("login.wrong.password@example.com", "wrong")
+    ).rejects.toEqual(
+      new ServerError(401, "Invalid email address or password")
+    );
+  });
 });
--- src/user/loginUser.ts
+++ src/user/loginUser.ts
@@ -1,6 +1,21 @@
-import { AuthTokens } from "./model";
+import { ServerError } from "../ServerError";
+import { withDatabase } from "../withDatabase";
+import { AuthTokens, makeAuthTokens } from "./model";
+import { compareSync } from "bcryptjs";
 
-export declare function loginUser(
+export async function loginUser(
   email: string,
   password: string
-): Promise<AuthTokens>;
+): Promise<AuthTokens> {
+  const error = "Invalid email address or password";
+
+  const user = await withDatabase((db) =>
+    db.user.findOne({ where: { email } })
+  );
+
+  if (!user || !compareSync(password, user.dataValues.password)) {
+    throw new ServerError(401, error);
+  }
+
+  return makeAuthTokens(user.dataValues);
+}
